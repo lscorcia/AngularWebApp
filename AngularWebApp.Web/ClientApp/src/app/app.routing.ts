a26e9b7f42ebf82c146eb567a005ae0100f7daf9
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2018-10-21 15:42:20 -0400

    Rimozione pagine demo

--- AngularWebApp.Web/ClientApp/src/app/app.routing.ts
+++ AngularWebApp.Web/ClientApp/src/app/app.routing.ts
@@ -55,32 +55,8 @@ export const routes: Routes = [
         loadChildren: './views/base/base.module#BaseModule'
       },
       {
-        path: 'buttons',
-        loadChildren: './views/buttons/buttons.module#ButtonsModule'
-      },
-      {
-        path: 'charts',
-        loadChildren: './views/chartjs/chartjs.module#ChartJSModule'
-      },
-      {
         path: 'dashboard',
         loadChildren: './views/dashboard/dashboard.module#DashboardModule'
-      },
-      {
-        path: 'icons',
-        loadChildren: './views/icons/icons.module#IconsModule'
-      },
-      {
-        path: 'notifications',
-        loadChildren: './views/notifications/notifications.module#NotificationsModule'
-      },
-      {
-        path: 'theme',
-        loadChildren: './views/theme/theme.module#ThemeModule'
-      },
-      {
-        path: 'widgets',
-        loadChildren: './views/widgets/widgets.module#WidgetsModule'
       }
     ]
   }
