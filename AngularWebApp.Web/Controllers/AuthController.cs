9b8b723087cc357dc206720b8513d97341ae5fed
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2018-10-23 16:50:18 -0400

    Fix: Riporta correttamente l'url dell'issuer

--- AngularWebApp.Web/Controllers/AuthController.cs
+++ AngularWebApp.Web/Controllers/AuthController.cs
@@ -31,9 +31,10 @@ namespace AngularWebApp.Web.Controllers
                 var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
                 var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
+                string currentUrl = this.Request.Scheme + "://" + this.Request.Host;
                 var tokeOptions = new JwtSecurityToken(
-                    issuer: "http://localhost:52582",
-                    audience: "http://localhost:52582",
+                    issuer: currentUrl,
+                    audience: currentUrl,
                     claims: new List<Claim>(),
                     expires: DateTime.Now.AddMinutes(5),
                     signingCredentials: signinCredentials
