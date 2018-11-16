3d49fca502d93108be42130f0be131f114e6d59b
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2018-11-16 13:37:22 -0500

    Fix build

--- AngularWebApp.Web/ClientApp/src/app/back-button.directive.spec.ts
+++ AngularWebApp.Web/ClientApp/src/app/back-button.directive.spec.ts
@@ -1,8 +1,9 @@
 import { BackButtonDirective } from './back-button.directive';
+import { TestBed } from '@angular/core/testing';
 
 describe('BackButtonDirective', () => {
   it('should create an instance', () => {
-    const directive = new BackButtonDirective();
+    const directive = TestBed.createComponent(BackButtonDirective);
     expect(directive).toBeTruthy();
   });
 });
